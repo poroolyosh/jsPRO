3d16e70a0f1ce827db90a6bc8c9d6dee6bfcb968
Poro <poroolyosh@gmail.com>
2023-11-13 20:59:38 +0300

    one lesson

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const isChecked = true;
-const isClose = true;
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: {},
+  privat: false,
+};
 
-console.log(!isChecked || isClose);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('На сколько оцените его?', ''),
+  c = prompt('Один из последних просмотренных фильмов?', ''),
+  d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
