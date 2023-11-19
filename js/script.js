a53866388273ed7e05c43e5ae74dd170b6410a47
Poro <poroolyosh@gmail.com>
2023-11-19 21:55:03 +0300

    24 практ 2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let result = '';
-const length = 11;
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: {},
+  privat: false,
+};
 
-for (let i = 1; i < length; i++) {
-  for (let j = 0; j < i; j++) {
-    result += '*';
-  }
-  result += '\n';
-}
-console.log(result);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('На сколько оцените его?', ''),
+  c = prompt('Один из последних просмотренных фильмов?', ''),
+  d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
