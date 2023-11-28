5ed95a7a7c406c4a02362645d6f1938c6eb75801
poroolyosh <poroolyosh@gmail.com>
2023-11-28 08:38:49 +0300

    467347347

--- js/script.js
+++ js/script.js
@@ -80,13 +80,35 @@ P.S. Функции вызывать не обязательно */
 
 // writeYourGenres();
 
-function learnJS(lang, callback) {
-  console.log(`Я учу ${lang}`);
-  callback();
-}
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  position: 'absolute',
+  colors: {
+    border: 'black',
+    bg: 'red',
+    background: 'white'
+  },
+  makeTest: function () {
+    console.log('Test');
+  }
+};
+options.makeTest();
 
-function done() {
-  console.log('Я прошел урок');
-}
+console.log(Object.keys(options));
 
-learnJS('JavaScript', done);
+// delete options.name;
+
+// console.log(options);
+
+for (let key in options) {
+  if (typeof (options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+  }
+
+}
