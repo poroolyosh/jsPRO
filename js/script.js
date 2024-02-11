387bd63b9e1e025e6f517a17e9b5725e67151d64
poroolyosh <poroolyosh@gmail.com>
2024-02-11 12:24:58 +0300

    1341234

--- js/script.js
+++ js/script.js
@@ -1,132 +1,14 @@
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно */
-
 'use strict';
 
-// let numberOfFilms;
+let salaries = {
+  John: 300,
+  Ann: 160,
+  Pete: 130
+};
 
-// function start() {
-//   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+}
 
-//   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//   }
-// }
-
-// start();
-
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false,
-// };
-
-// function rememberMyFilms() {
-//   for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
-//       b = prompt('На сколько оцените его?', '').trim();
-
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//       personalMovieDB.movies[a] = b;
-//       console.log('done');
-//     } else {
-//       console.log('error');
-//       i--;
-//     }
-//   }
-// }
-
-// rememberMyFilms();
-
-// function detectPersonalLevel() {
-//   if (personalMovieDB.count < 10) {
-//     console.log('Просмотрено довольно мало фильмов');
-//   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//     console.log('Вы классический зритель');
-//   } else if (personalMovieDB.count >= 30) {
-//     console.log('Вы киноман');
-//   } else {
-//     console.log('Произошла ошибка');
-//   }
-// }
-
-// detectPersonalLevel();
-
-// function showMyDB(hidden) {
-//   if (!hidden) {
-//     console.log(personalMovieDB);
-//   }
-// }
-
-// showMyDB(personalMovieDB.privat);
-
-// function writeYourGenres() {
-//   for (let i = 1; i <= 3; i++) {
-//     personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-//   }
-// }
-
-// writeYourGenres();
-
-// const options = {
-//   name: 'test',
-//   width: 1024,
-//   height: 1024,
-//   position: 'absolute',
-//   colors: {
-//     border: 'black',
-//     bg: 'red',
-//     background: 'white'
-//   },
-//   makeTest: function () {
-//     console.log('Test');
-//   }
-// };
-// options.makeTest();
-
-// console.log(Object.keys(options));
-
-// // delete options.name;
-
-// // console.log(options);
-
-// for (let key in options) {
-//   if (typeof (options[key]) === 'object') {
-//     for (let i in options[key]) {
-//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//     }
-//   } else {
-//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//   }
-
-// }
-
-const arr = [1, 3, 8, 9, 12, 47];
-
-arr.forEach(function (item, i, arr) {
-  console.log(`${i}: ${item} внутри массива ${arr}`);
-});
-
-// arr.push(58);
-
-// console.log(arr);
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
-
-// for (let value of arr) {
-//   console.log(value);
-// }
+alert(sum); // 390
